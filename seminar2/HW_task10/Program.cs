04a6f596a185abd113c735f5d95d67de3918e5e1
Ivan <coltwalker@mail.ru>
2022-09-03 00:31:54 +0500

    realized HWtask10

---
+++ seminar2/HW_task10/Program.cs
@@ -0,0 +1,21 @@
+﻿//  Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите трёхзначное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 0)
+{
+    number = -number;
+}
+if (number > 99 && number < 1000) 
+{
+    int secNumber = number / 10;
+    int res = secNumber % 10; 
+    Console.WriteLine($"Вторая цифра {res}");
+}
+else
+{
+   Console.WriteLine("Было введено не трёхзначное число");
+}
