2019cc0dd44601b9e0d038ee0f67b68ee1cea594
Ivan <coltwalker@mail.ru>
2022-09-19 23:51:28 +0500

    seminar5HW

---
+++ seminar5/HW_task34/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int[] GetArray(int size, int leftRange, int rightRange)
+{
+    int[] arr = new int[size];
+    
+    Random rand = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rand.Next(leftRange, rightRange + 1);
+    }
+    return arr;
+}
+int CountEvenElem(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+
+int[] array = GetArray(20, 100, 999);
+Console.WriteLine(string.Join(", ", array));
+Console.WriteLine($"Количество чётных чисел в массиве равно {CountEvenElem(array)}");
---
+++ seminar5/HW_task38/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задайте массив натуральных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+int[] GetArray(int size, int leftRange, int rightRange)
+{
+    int[] arr = new int[size];
+    Random rand = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rand.Next(leftRange, rightRange + 1);
+    }
+    return arr;
+}
+
+int DiffMaxMin(int[] arr)
+{
+    int max = arr[0];
+    int min = arr[0];
+    for (int i = 1; i < arr.Length; i ++)
+    {
+        if (arr[i] > max)
+        {
+            max = arr[i];
+            // Console.WriteLine($"макс = {max}");
+        }
+        else if (arr[i] < min)
+        {
+            min = arr[i];
+            // Console.WriteLine($"мин = {min}");
+        }
+    }
+    Console.WriteLine($"максимальный элемент равен {max}");
+    Console.WriteLine($"минимальный элемент равен {min}");
+    int diff = max - min;
+    return diff;
+}
+
+int[] array = GetArray(20, 1, 100);
+Console.WriteLine(string.Join(" ", array));
+Console.WriteLine($"Разница между максимальным и минимальным элементами массива равна {DiffMaxMin(array)}");
