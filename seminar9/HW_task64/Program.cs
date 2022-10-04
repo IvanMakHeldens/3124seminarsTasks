ea87d559868aeb5b4faa1a1132c5699a9aed7a15
Ivan <coltwalker@mail.ru>
2022-10-04 19:01:05 +0500

    HWSem9

---
+++ seminar9/HW_task64/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+
+int ReadNumber (string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+} 
+
+void PrintNto1(int n, int m)
+{
+    
+    if (m > n)
+    {
+        return;
+    }
+    else
+    {
+        PrintNto1(n, m + 1);
+        Console.Write(m + " ");
+    }
+}
+
+int number = ReadNumber("Введите N: ");
+int m = 1;
+PrintNto1(number, m);
---
+++ seminar9/HW_task66/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int ReadNumber (string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+} 
+
+int SumMToN(int m, int n)
+{   
+    
+    if(n < m)
+    {
+        return 0;
+    }
+    else
+    
+    {
+        return SumMToN(m, n-1) + n;
+    }
+    
+}
+int M = ReadNumber("Введите M: ");
+int N = ReadNumber("Введите N: ");
+Console.WriteLine(SumMToN(M, N));  
